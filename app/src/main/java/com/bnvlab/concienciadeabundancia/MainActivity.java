9602d8aa58c9bd31dd83c9d6569261ed902a388c
Marcos Vazquez <mjv87ster@gmail.com>
2017-03-16 23:45:25 -0300

    Agregé el layout del login con el que voy a trabajar. Lean las reglas en el MainActivity.java

--- app/src/main/java/com/bnvlab/concienciadeabundancia/MainActivity.java
+++ app/src/main/java/com/bnvlab/concienciadeabundancia/MainActivity.java
@@ -1,13 +1,36 @@
 package com.bnvlab.concienciadeabundancia;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+/*
+NORMAS:
+    -DESCRIPCIÓN DE MÉTODOS, SUS PARÁMETROS Y DEMÁS
+    -COMENTARIOS
+    -NOMBRES DE FUNCIONES Y VARIABLES AUTODESCRIPTIVOS - PREFERENTEMENTE EN INGLÉS
+    -CADA CLASE DEBE TENER SU COMENTARIO DESCRIBIENDO EL PROPÓSITO
+    -EN MÉTODOS COMPLICADOS, ANOTAR EL PSEUDOCÓDICO EN UN COMENTARIO
+    -CREAR CÓDIGO LO MÁS GENÉRICO POSIBLE, PARA PODER REUTILIZARLO
+    -CADA COMMIT DEBE IR ACOMPAÑADO DE UNA DESCRIPCIÓN DE LO QUE SE HIZO Y DE LO QUE SE SE VA A SEGUIR EN EL PRÓXIMO
+    -LOS TEXTOS, COLORES Y DEMÁS EN SU RESPECTIVO XML
+*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ESTO ES SIMPLEMENTE PARA PROBAR EL LOGIN
+        Button buttonTestLogin = (Button) findViewById(R.id.button_login_test);
+        buttonTestLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.layout_login);
+            }
+        });
+
     }
 }
